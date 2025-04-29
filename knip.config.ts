0fa7b887aab35ee868996403b4737053bc466471
Johannes Stein <johannesstein@freeze-dev.com>
2025-04-30 00:36:34 +0100

    Fix knip issues

--- knip.config.ts
+++ knip.config.ts
@@ -1,4 +1,10 @@
 export default {
-  entry: ["index.{js,ts}", "src/index.{js,ts}", "spec/**/*.{js,ts}"],
+  entry: [
+    "index.{js,ts}",
+    "src/index.{js,ts}",
+    "spec/**/*.{js,ts}",
+    "bin/index.{js,ts}",
+  ],
   project: ["**/*.{js,ts}"],
+  ignoreBinaries: ["eslint"],
 };
